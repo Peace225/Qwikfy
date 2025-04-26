8f9a20cbd4ac50236b21bba82cf73c2bbd998b2a
Brad <bradfalconnek@gmail.com>
2025-04-26 04:17:38 +0100

    Fix vite.config.js - ajout base pour corriger la page blanche

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ Ajoute ceci pour éviter la page blanche sur Vercel
   plugins: [react()],
 })
